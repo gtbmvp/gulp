8d681fd7470c70fd089af365674b1a5e67a74d25
grindingeorge <grindingeorge@gmail.com>
2021-12-20 11:43:01 +0300

    remove concat and cssimport from sass task pipe

--- gulpfile.js/tasks/sass.js
+++ gulpfile.js/tasks/sass.js
@@ -1,7 +1,5 @@
 //PACKAGES
 const { src, dest } = require("gulp");
-const concat = require("gulp-concat");
-const cssimport = require("gulp-cssimport");
 const autoprefixer = require("gulp-autoprefixer");
 const csso = require("gulp-csso");
 const rename = require("gulp-rename");
@@ -15,8 +13,6 @@ const config = require("../config");
 // SASS TASK
 module.exports = () => {
   return src(config.sass.src, { sourcemaps: config.isDevelopment })
-    .pipe(concat("main.css"))
-    .pipe(cssimport())
     .pipe(sass())
     .pipe(webpCss())
     .pipe(autoprefixer())
