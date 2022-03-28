0403b8d74186dbf18820bbdbf6428b6b94126c95
grindingeorge <grindingeorge@gmail.com>
2022-03-28 17:43:57 +0300

    Update JS task, exclude gulp-babel, use webpack with babel-loader

--- README.md
+++ README.md
@@ -35,8 +35,8 @@
   - group CSS media queries at the end of styles;
   - saving both minified and not minified versions;
 - JS:
-  - babel with _@babel/preset-env_ preset;
   - webpack bundling with minification (if _production_);
+  - webpack babel-loader with _@babel/preset-env_ preset;
 - FONT:
   - convert ttf to woff and woff2;
   - generate CSS file with @font-face rules;
--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -1,6 +1,5 @@
 //PACKAGES
 const { src, dest } = require("gulp");
-const babel = require("gulp-babel");
 const webpack = require("webpack-stream");
 
 //CONFIG
@@ -9,10 +8,26 @@ const config = require("../config");
 // JS TASK
 module.exports = () => {
   return src(config.js.src, { sourcemaps: config.isDevelopment })
-    .pipe(babel())
     .pipe(
       webpack({
+        module: {
+          rules: [
+            {
+              test: /\.m?js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: "babel-loader",
+                options: {
+                  presets: ["@babel/preset-env"],
+                },
+              },
+            },
+          ],
+        },
         mode: config.isDevelopment ? "development" : "production",
+        output: {
+          filename: "bundle.js",
+        },
       })
     )
     .pipe(dest(config.js.public, { sourcemaps: config.isDevelopment }));
